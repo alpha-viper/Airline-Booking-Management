668e230f6c365b63005088ed4b184f81f09b978b
alpha-viper <sumitmishra2711@gmail.com>
2022-12-15 17:53:23 +0530

    Added city model and migration

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
@@ -6,7 +7,8 @@ console.log(PORT);
 const setupAndStartServer = async = ()=>{
 
     const app=express();
-    
+    app.use(bodyParser.json);
+    app.use(bodyParser.urlencoded({extended: 'true'}));    
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
         //console.log(process.env);
