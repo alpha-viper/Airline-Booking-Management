7675c16fb1f7e20709d9280ee6fad38eae0e7495
alpha-viper <sumitmishra2711@gmail.com>
2022-12-16 22:01:23 +0530

    Completed basic city repository

---
+++ FlightsAndSearch/src/repository/city-repository.js
@@ -0,0 +1,54 @@
+const City=require('../models/index');
+
+class CityRepository{
+
+   async createCity({ name })
+   {
+    try {
+        const city = await City.create({ 
+            name 
+        });
+        return city;
+    } catch (error) {
+        throw { error };
+    }
+   }
+
+   async deleteCity(cityId)
+   {
+    try {
+        await City.destroy({
+            where:{
+                id: cityId
+            }
+        })
+        return true;
+    } catch (error) {
+        throw { error };
+    }
+   }
+   async updateCity(cityId,data)
+   {
+    try {
+        const city= await City.update(data,{
+            where:{
+                id: cityId
+            }
+        })
+        return city;
+    } catch (error) {
+        throw {error};
+    }
+   }
+   async getCity(cityId)
+   {
+    try {
+        const city=await City.findByPK(cityId);
+        return city;
+    } catch (error) {
+        throw {error};
+    }
+   }
+}
+
+module.exports = CityRepository;
